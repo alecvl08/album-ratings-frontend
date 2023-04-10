3ded682c6271e2851bf3e8e9eb1ec2f3e3356fc2
Alec VanLandingham <alecvl08@vt.edu>
2023-04-09 23:27:51 -0400

    slight issue with range input

--- src/globalVars.js
+++ src/globalVars.js
@@ -1,2 +1,2 @@
-const apiBasePath = process.env.REACT_APP_API_BASE_PATH || 'https://album-ratings-backend.herokuapp.com'
+const apiBasePath = process.env.REACT_APP_API_BASE_PATH
 export default apiBasePath
